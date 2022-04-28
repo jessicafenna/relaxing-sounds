c47e17fddb73dca0e643da771b39ee3181fe06ad
Jessica Fenna <jessicafenna@gmail.com>
2022-04-28 18:38:06 +0100

    2nd commit

--- app.js
+++ app.js
@@ -11,26 +11,22 @@ const sounds = [
  'sea waves',
  'rain'
 ]
+const keys =
+[
+  'q',
+  'w',
+  'e',
+  'r'
+];
 
-// function playSounds()
-//   sounds.forEach(sound =>{
-//     const sound = document.getElementById(sound);
-//     // var mySound= document.getElementById("sounds");
-//       if(sound.paused)
-//       {
-//         sound.play();
-//       }
-//       else
-//       {
-//         sound.pause();
-//       }
-//   });
+
+
 
 // function for click to play sounds
 sounds.forEach((sound) => { 
   const btn = document.createElement ('button');
   btn.classList.add('btn');
-  btn.innerText = sound;
+  btn.innerText = sound, `Press ${keys}`;
 
   btn.addEventListener("click", () =>{ 
    
@@ -39,15 +35,35 @@ sounds.forEach((sound) => {
   });
 
   document.getElementById('buttons').appendChild(btn);
-
+  
 });
+
+// document.addEventListener("keydown", function(e) {
+//     if (e.keyCode == 81) {
+//       document.getElementById("audio").play();
+//     }
+//   });
+
 
 // function to stop last sound from playing when clicking next
 function stopSongs(){ 
   sounds.forEach(sound =>{ 
     const song = document.getElementById(sound);
     song.pause();
-    song.currentTime = 0;
+    // song.currentTime = 0;
   });
 }
+
+// sounds.forEach((sound) => {
+//     btn.addEventListener("click", () =>{ 
+//       if(sound == sound.paused)
+//       {
+//         sound.play();
+//       }
+//       else
+//       {
+//         sound.pause();
+//       }
+//   });
+//   });
 
