6d76077abfd816ffedd1a858568b41a3e61b2dc8
Jessica Fenna <jessicafenna@gmail.com>
2022-04-29 14:56:58 +0100

    added style and more sounds

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
-// document.addEventListener("keydown", function(e) {
-//     if (e.keyCode == 81) {
-//       document.getElementById("audio").play();
-//     }
-//   });
-
 // Sounds stored in an array, called in next function 
 const sounds = [ 
- 'birds',
- 'rainforest',
- 'sea waves',
- 'rain'
+ 'birds - click or press q',
+ 'rainforest - click here or press w',
+ 'sea waves - click here or press e',
+ 'rain - click here or press r',
+ 'tropical rain storm - click here or press t'
 ]
-const keys =
-[
-  'q',
-  'w',
-  'e',
-  'r'
-];
-
-
-
 
 // function for click to play sounds
 sounds.forEach((sound) => { 
   const btn = document.createElement ('button');
   btn.classList.add('btn');
-  btn.innerText = sound, `Press ${keys}`;
+  btn.innerText = sound;
 
   btn.addEventListener("click", () =>{ 
    
-    stopSongs();
+    // stopSongs();
     document.getElementById(sound).play();
   });
 
@@ -38,19 +23,34 @@ sounds.forEach((sound) => {
   
 });
 
-// document.addEventListener("keydown", function(e) {
-//     if (e.keyCode == 81) {
-//       document.getElementById("audio").play();
-//     }
-//   });
 
+document.addEventListener("keydown", function(event) {
+  if (event.keyCode == 81) {
+    // stopSongs();
+    document.getElementById("birds - click or press q").play();
+  }
+  else if  (event.keyCode == 87) {
+    // stopSongs();
+    document.getElementById("rainforest - click here or press w").play();
+  }
+  else if (event.keyCode == 69) {
+    // stopSongs();
+    document.getElementById("sea waves - click here or press e").play();
+  }
+  else if (event.keyCode == 82){ 
+    document.getElementById("rain - click here or press f").play();
+  }
+  else if (event.keyCode == 84) { 
+    document.getElementById("tropical rain storm - click here or press t").play()
+  }
+});
 
 // function to stop last sound from playing when clicking next
 function stopSongs(){ 
   sounds.forEach(sound =>{ 
     const song = document.getElementById(sound);
     song.pause();
-    // song.currentTime = 0;
+    song.currentTime = 0;
   });
 }
 
